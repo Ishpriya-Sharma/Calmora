665d273df3a92c1d7036fd175396423176f0dabb
Ishpriya <ishpriyasharma15@gmail.com>
2026-04-16 12:51:51 +0530

    Comment out auth and journal routes
    Comment out routes and MongoDB connection for testing.

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 //const mongoose = require("mongoose");
 const cors = require("cors");
 
-const authRoutes = require("./routes/auth");
-const journalRoutes = require("./routes/journal");
+//const authRoutes = require("./routes/auth");
+//const journalRoutes = require("./routes/journal");
 
 const app = express();
 
@@ -13,7 +13,10 @@ app.use(express.static("public"));
 
 //mongoose.connect("mongodb://127.0.0.1:27017/calmora");
 
-app.use("/api/auth", authRoutes);
-app.use("/api/journal", journalRoutes);
+//app.use("/api/auth", authRoutes);
+//app.use("/api/journal", journalRoutes);
+app.get("/", (req, res) => {
+  res.send("Calmora backend working 🚀");
+});
 
 app.listen(5000, () => console.log("Server running on port 5000"));
